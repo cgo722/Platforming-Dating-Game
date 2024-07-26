0e92f75be2d5aaf8ea1c6395d1591c1d524c3819
CamronLinux <holy_hades@ymail.com>
2024-07-26 12:43:47 -0600

    ?

--- PlatformingDatingGame/Scripts/DialogueSelector.gd
+++ PlatformingDatingGame/Scripts/DialogueSelector.gd
@@ -2,7 +2,7 @@ extends Button
 class_name LvlSelc
 
 @export var dialogueOption : int
-
+ 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.

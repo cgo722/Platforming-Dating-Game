42d99b120e31ceafd51716e946eaca16570eb0bb
Camron <holy_hades@ymail.com>
2024-07-25 18:49:44 -0600

    Started level spawing

--- PlatformingDatingGame/Scripts/DialogueSelector.gd
+++ PlatformingDatingGame/Scripts/DialogueSelector.gd
@@ -2,6 +2,7 @@ extends Button
 class_name LvlSelc
 
 @export var dialogueOption : int
+@export var levelSelect : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,3 +12,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_button_down():
+	var loadLevel = levelSelect.instantiate()
+	get_node(".").add_child(loadLevel)

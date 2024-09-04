940015ed205f48b369188cb537e5ce3bf188ea65
Camron <holy_hades@ymail.com>
2024-09-04 10:48:52 -0600

    Added a new camera addon and fleshed out the level 1 some more

--- PlatformingDatingGame/Scenes/PlatformingCharacter.gd
+++ PlatformingDatingGame/Scenes/PlatformingCharacter.gd
@@ -4,10 +4,10 @@ var speed : float
 @export var walkSpeed : float = 300.0
 @export var runSpeed : float = 500.0
 @export var jumpVel : float = -400.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $CollisionShape2D/AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,7 +17,9 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = jumpVel
-		
+	
+	if Input.is_action_just_released("Jump") and velocity.y < 0:
+		velocity.y = -10
 	if Input.is_action_pressed("Sprint"):
 		speed = runSpeed
 
@@ -27,7 +29,13 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * speed
 		speed = walkSpeed
+		animated_sprite_2d.play("Run")
+		if velocity.x < 0:
+			animated_sprite_2d.flip_h = true
+		else:
+			animated_sprite_2d.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+		animated_sprite_2d.play("Idle")
 
 	move_and_slide()

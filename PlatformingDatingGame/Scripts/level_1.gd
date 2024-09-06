69e928468e4f2ddb6c5d3022d0aa197b74c77ba3
Camron <holy_hades@ymail.com>
2024-09-06 09:02:22 -0600

    Added a gamemanager

--- PlatformingDatingGame/Scripts/level_1.gd
+++ PlatformingDatingGame/Scripts/level_1.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var timer : float
-@export_range(0, 3) var charisma : int
+@onready var gamemanager = get_node("/root/GameManager")
 @export var player : CharacterBody2D
 var startLocation : Vector2
 # Called when the node enters the scene tree for the first time.
@@ -13,20 +13,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	timer += delta
-	
-	pass
+
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if timer < 5:
-		charisma = 3
+		gamemanager.charisma = 3
 	else: if timer < 10:
-		charisma = 2
+		gamemanager.charisma = 2
 	else: if timer < 20:
-		charisma = 1
+		gamemanager.charisma = 1
 	else: if timer > 20:
-		charisma = 0
-	print(charisma)
+		gamemanager.charisma = 0
 	pass # Replace with function body.
 
 func _on_world_boundry_body_entered(body: Node2D) -> void:
